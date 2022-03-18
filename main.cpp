b0f9f092f6715119dd14678b978e7735321707f3
Deven <dellis1@ufl.edu>
2022-03-18 11:49:07 -0400

    added skeletons

--- main.cpp
+++ main.cpp
@@ -5,9 +5,19 @@
 
 using namespace std;
 
-vector<string> fileParse(string filename) {
+vector<string> fileParse(string filename) {//returns a vector of the objectives the user wants to compare against
 
 }
+
+vector<string> csvParse(string filename) { //returns the responses of the students as a vector of strings
+
+}
+
+void rank(vector<string> csvVec, vector<string> fileVec) { //modifies the fileVec to be in order of the rank computed
+
+}
+
+
 
 int main() {
 	cout << "Hello world!";
