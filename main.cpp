b58fdd9e1877e9fecae2ca61538529f92a00569b
Zhiqing Qu <susanqu9999@outlook.com>
2022-03-22 14:19:51 -0400

    Update main.cpp
    Half way finish the csvParse() function.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -10,6 +11,17 @@ vector<string> fileParse(string filename) {//returns a vector of the objectives 
 }
 
 vector<string> csvParse(string filename) { //returns the responses of the students as a vector of strings
+	vector<pair<string, vector<int>>> studentResponse; //Store the result.
+	ifstream csvFile(filename); 
+
+	if(!csvFile.is_open()) //Make sure if the file is opened.
+		throw runtime_error("CSV file cannot be opened!"); 
+	
+	string line;	
+	if(csvFile.good()){
+		getline(csvFile, line); //Extract the first line of the file.
+
+	}
 
 }
 
